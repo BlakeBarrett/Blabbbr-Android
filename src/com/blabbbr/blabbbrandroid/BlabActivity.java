dcd805825f509c4d920520066fda10369c9df194
Blake Barrett <blake@blakebarrett.net>
2014-01-17 23:48:35 -0800

    Blabbbr records audio for 5 seconds and saves a .3gp file on your SD card in the "Ringtones" folder.

--- src/com/blabbbr/blabbbrandroid/BlabActivity.java
+++ src/com/blabbbr/blabbbrandroid/BlabActivity.java
@@ -1,37 +1,66 @@
 package com.blabbbr.blabbbrandroid;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.Button;
 
 public class BlabActivity extends Activity {
 
-	static final String TAG = "BlabActivity";
+	static final String TAG = BlabActivity.class.toString();
 	private static Recorder recorder;
-	
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_blab);
-        findViewById(R.id.button1).setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-            	if (recorder.getIsRecording()) {
-            		recorder.stopRecording();
-            	} else {
-            		recorder.startRecording();
-            	}
-            }
-        });
-        recorder = new Recorder();
-    }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.blab, menu);
-        return true;
-    }
-    
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_blab);
+		final Button recordButton = (Button) findViewById(R.id.button1);
+		recordButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				recorder.toggleRecording();
+			}
+		});
+
+		final String filePath = Environment.getExternalStoragePublicDirectory(
+				Environment.DIRECTORY_RINGTONES).getAbsolutePath();
+		Log.d(TAG, filePath);
+		recorder = new Recorder(filePath);
+
+		recorder.setOnStartRecordingCallback(new Runnable() {
+			@Override
+			public void run() {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						recordButton.setBackgroundColor(Color.RED);
+					}
+				});
+			}
+		});
+		recorder.setOnStopRecordingCallback(new Runnable() {
+
+			@Override
+			public void run() {
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						recordButton.setBackgroundColor(Color.GRAY);
+					}
+				});
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.blab, menu);
+		return true;
+	}
 
 }
--- src/com/blabbbr/blabbbrandroid/Recorder.java
+++ src/com/blabbbr/blabbbrandroid/Recorder.java
@@ -1,52 +1,108 @@
 package com.blabbbr.blabbbrandroid;
 
 import android.media.MediaRecorder;
+import android.os.Handler;
 import android.text.format.Time;
 import android.util.Log;
 
 public class Recorder {
-	
-	public Recorder() {
+
+	private static final String TAG = Recorder.class.toString();
+	public static final int BLAB_LENGTH_SECONDS = 6;
+
+	public static int AUDIO_SOURCE = MediaRecorder.AudioSource.CAMCORDER;
+	public static int AUDIO_OUTPUT_FORMAT = MediaRecorder.OutputFormat.THREE_GPP;
+	public static int AUDIO_ENCODER = MediaRecorder.AudioEncoder.AMR_WB;
+
+	private MediaRecorder recorder;
+
+	public Recorder(final String filePath) {
 		super();
-		setupAudio();
+		setupAudio(filePath);
 	}
-	
-    MediaRecorder recorder;
-    private void setupAudio() {
-    	Time today = new Time(Time.getCurrentTimezone());
-    	today.setToNow();
-    	String fileLocation = "" + today.toString();
-    	
-    	// W00T!!!
-    	recorder = new MediaRecorder();
-    	try {
-    		recorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-    		recorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-    		recorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
-    		recorder.setOutputFile(fileLocation);
-    		recorder.prepare();
-    	} catch (Exception e) {
-    		Log.d(Recorder.class.toString(), e.toString());
-    		// don't care
-    	}
-    }
-    
-    private Boolean isRecording = false;
-    Boolean getIsRecording() {
-    	return isRecording;
-    }
-    
-    void startRecording() {
-    	if (isRecording) {
-    		stopRecording();
-    	}
-    	recorder.start();
-    	isRecording = true;
-    }
-    
-    void stopRecording() {
-    	recorder.stop();
-    	recorder.release();
-    	isRecording = false;
-    }
+
+	private void setupAudio(final String filePath) {
+		final Time today = new Time(Time.getCurrentTimezone());
+		today.setToNow();
+		final String fileLocation = filePath + "/"
+				+ String.valueOf(today.toMillis(false)) + "_blab.3gp";
+
+		recorder = new MediaRecorder();
+		try {
+			recorder.setAudioSource(AUDIO_SOURCE);
+			recorder.setOutputFormat(AUDIO_OUTPUT_FORMAT);
+			recorder.setAudioEncoder(AUDIO_ENCODER);
+			recorder.setOutputFile(fileLocation);
+			recorder.prepare();
+			Log.d(TAG, "ready");
+		} catch (Exception e) {
+			Log.d(TAG, e.toString());
+		}
+	}
+
+	private Boolean isRecording = false;
+
+	Boolean getIsRecording() {
+		return isRecording;
+	}
+
+	public void startRecording() {
+		if (isRecording) {
+			stopRecording();
+		}
+		recorder.start();
+		isRecording = true;
+		stopRecordingAfterInterval();
+		if (onStartRecordingCallback != null) {
+			onStartRecordingCallback.run();
+		}
+		Log.d(TAG, "recording");
+	}
+
+	public void stopRecording() {
+		recorder.stop();
+		recorder.reset();
+		recorder.release();
+		isRecording = false;
+		if (handler != null) {
+			handler.removeCallbacks(stopRecordingRunnable);
+		}
+		if (onStopRecordingCallback != null) {
+			onStopRecordingCallback.run();
+		}
+		Log.d(TAG, "stopped");
+	}
+
+	public void toggleRecording() {
+		if (getIsRecording()) {
+			stopRecording();
+		} else {
+			startRecording();
+		}
+	}
+
+	private Runnable onStopRecordingCallback;
+
+	public void setOnStopRecordingCallback(final Runnable runnable) {
+		this.onStopRecordingCallback = runnable;
+	}
+
+	private Runnable onStartRecordingCallback;
+
+	public void setOnStartRecordingCallback(final Runnable runnable) {
+		this.onStartRecordingCallback = runnable;
+	}
+
+	private Handler handler;
+	private final Runnable stopRecordingRunnable = new Runnable() {
+		@Override
+		public void run() {
+			stopRecording();
+		}
+	};
+
+	private void stopRecordingAfterInterval() {
+		handler = new Handler();
+		handler.postDelayed(stopRecordingRunnable, (BLAB_LENGTH_SECONDS * 1000));
+	}
 }
